1ab3c7737a66c5752f2e21c1cafdd5ae7b786651
Nahid Hasan <nahidn228@gmail.com>
2025-02-14 18:59:13 +0600

    show singale image with details

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { Route, Routes } from "react-router";
 import AppLayout from "./Layout/AppLayout";
-import Home from "./Pages/Home";
 import Create from "./Pages/Create";
+import Creation from "./Pages/Creation";
+import Home from "./Pages/Home";
+import SingleImage from "./Pages/SingleImage";
 
 function App() {
   return (
@@ -10,6 +12,11 @@ function App() {
         <Route path="/" element={<AppLayout></AppLayout>}>
           <Route index element={<Home></Home>}></Route>
           <Route path="/create" element={<Create></Create>}></Route>
+          <Route path="/creations" element={<Creation> </Creation>}></Route>
+          <Route
+            path="/creation/:id"
+            element={<SingleImage></SingleImage>}
+          ></Route>
         </Route>
       </Routes>
     </>
---
+++ src/Pages/Creation.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router";
+import PageTitle from "./../components/shared/PageTitle";
+
+const Creation = () => {
+  const [images, setImages] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/api/v1/image/all")
+      .then((res) => res.json())
+      .then((data) => setImages(data));
+  }, []);
+  return (
+    <div>
+      <PageTitle>All Creation</PageTitle>
+      <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5 my-10">
+        {images.map((img) => (
+          <div key={img._id}>
+            <div className="card card-compact bg-base-100 shadow-xl relative">
+              <figure>
+                <img src={img.thumb_img} className="w-full" alt="Shoes" />
+              </figure>
+
+              <Link
+                to={`/creation/${img._id}`}
+                className="btn btn-primary shadow-2xl absolute bottom-1 right-1"
+              >
+                Details
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Creation;
