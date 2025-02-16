60ce8ce2b77a26280bb6a5f2498a50d5146b2818
Nahid Hasan <nahidn228@gmail.com>
2025-02-16 23:46:55 +0600

    show user comment an AI reply

--- src/Pages/SingleImage.jsx
+++ src/Pages/SingleImage.jsx
@@ -23,18 +23,24 @@ const SingleImage = () => {
       imageId: id,
       comment,
     };
-    console.log(document);
 
     axios
       .post("http://localhost:3000/api/v1/comment/create", document)
       .then((res) => console.log(res.data));
+    e.target.reset();
   };
 
   useEffect(() => {
     fetch(`http://localhost:3000/api/v1/image/single/${id}`)
       .then((res) => res.json())
       .then((data) => setImage(data));
+
+    axios
+      .get("http://localhost:3000/api/v1/comment/comments")
+      .then((res) => setComments(res.data));
   }, [id]);
+
+  console.log(comments);
   return (
     <div>
       <PageTitle>{image?.prompt}</PageTitle>
@@ -43,22 +49,31 @@ const SingleImage = () => {
           <img src={image?.original_img} alt="" className="rounded-md" />
         </figure>
         {/* comment */}
-        <div className="max-w-2xl mx-auto p-4 bg-gray-900 text-white rounded-lg shadow-lg">
+        <div className="max-w-2xl mx-auto p-4 bg-gray-900 text-white rounded-lg shadow-lg max-h-screen overflow-y-scroll">
           {/* Comment List */}
           <div className="mt-4 space-y-3 border-b border-gray-700 pb-3 ">
-            {comments.map((comment, index) => (
+            {comments?.map((comment, index) => (
               <div
                 key={index}
-                className={`flex items-start gap-3 p-2 rounded-lg ${
-                  comment.user === "AI" ? "bg-gray-800" : "bg-gray-700"
-                }`}
+                className={`flex flex-col items-start gap-3 p-2 rounded-lg `}
               >
-                <div className="p-2 bg-gray-600 rounded-full">
-                  {comment.user === "AI" ? <FaRobot /> : <FaUser />}
+                {/* user comment */}
+                <div className="flex gap-2">
+                  <div className="p-2 bg-gray-600 rounded-full">
+                    <FaUser />
+                  </div>
+                  <div>
+                    <p className="font-bold">{comment.comment}</p>
+                  </div>
                 </div>
-                <div>
-                  <p className="font-bold">{comment.user}</p>
-                  <p className="text-sm">{comment.text}</p>
+                {/* AI Reply*/}
+                <div className="flex gap-2">
+                  <div className="p-2 bg-gray-600 rounded-full">
+                    <FaRobot />
+                  </div>
+                  <div>
+                    <p className="text-sm">{comment.reply}</p>
+                  </div>
                 </div>
               </div>
             ))}
